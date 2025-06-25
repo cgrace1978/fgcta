8d14b7084dadc23f5b7c6575e01bc11065b12f85
Chris Grace <cgrace@well.ox.ac.uk>
2025-06-25 13:25:31 +0100

    Update compute_ldscores.bash

--- polyfun/compute_ldscores.bash
+++ polyfun/compute_ldscores.bash
@@ -1,18 +1,12 @@
 #!/bin/bash
 
-# python compute_ldscores_from_ld.py \
-#   --ukb \
-#   --out output/test_weight.parquet
+## generates baseline ld scores from UKB genotype data
 
-#  --annot example_data/annotations.1.annot.parquet \
-
-## 
 chr=$1
 
 python compute_ldscores.py \
        --bfile ../LD_new/ukb_v3_maf_imp_${chr}_eur_wcm  \
        --out output/ukb_chr${chr}.parquet
 
-#--annot ../SKI_annot_tmp1 \
 
 
