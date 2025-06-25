339b1e5825166f76f5d104603ed27a2c0333b89d
Chris Grace <cgrace@well.ox.ac.uk>
2025-06-25 13:38:02 +0100

    Add files via upload

---
+++ carma/slurm/gen_assoc_file.sh
@@ -0,0 +1,25 @@
+#!/bin/bash 
+
+#SBATCH -p short
+#SBATCH --mem 32000
+ 
+echo "------------------------------------------------" 
+echo "Run on host: "`hostname` 
+echo "Operating system: "`uname -s` 
+echo "Username: "`whoami` 
+echo "Started at: "`date` 
+echo "------------------------------------------------" 
+ 
+date
+
+FIRST=$SLURM_ARRAY_TASK_ID
+
+LAST=$(($FIRST + $SLURM_ARRAY_TASK_STEP - 1))
+
+for (( MY_TASK=$FIRST; MY_TASK<=$LAST; MY_TASK++ ))
+do
+    perl gen_assoc_file.pl $MY_TASK
+done
+
+
+date
